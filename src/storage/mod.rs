31f4455603c163844fe515f80babb751e43c9651
Santiago Carmuega <santiago@carmuega.me>
2022-11-24 13:11:28 -0300

    feat: Introduce storage module

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,3 +1,26 @@
+pub mod blocks;
+pub mod chain;
 mod cursor;
 
 pub use cursor::*;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("serde error {0}")]
+    Serde(String),
+
+    #[error("storage error {0}")]
+    Storage(String),
+}
+
+impl Error {
+    fn serde(error: impl ToString) -> Self {
+        Self::Serde(error.to_string())
+    }
+
+    fn storage(error: impl ToString) -> Self {
+        Self::Storage(error.to_string())
+    }
+}
