7ee17916b0f9db675cd1fc549d6e17a469fb0913
Santiago Carmuega <santiago@carmuega.me>
2022-11-24 11:57:48 +0100

    feat: Introduce ChainSync stage (#2)

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,1 +1,66 @@
-// TODO
+pub use super::model::*;
+pub use super::storage::Cursor;
+
+use std::fmt::Display;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("configuration error: {0}")]
+    ConfigError(String),
+
+    #[error("client error: {0}")]
+    ClientError(String),
+
+    #[error("parse error: {0}")]
+    ParseError(String),
+
+    #[error("server error: {0}")]
+    ServerError(String),
+
+    #[error("storage error: {0}")]
+    StorageError(String),
+
+    #[error("{0}")]
+    Message(String),
+
+    #[error("{0}")]
+    Custom(String),
+}
+
+impl Error {
+    pub fn config(text: impl Into<String>) -> Error {
+        Error::ConfigError(text.into())
+    }
+
+    pub fn client(error: impl Display) -> Error {
+        Error::ClientError(error.to_string())
+    }
+
+    pub fn parse(error: impl Display) -> Error {
+        Error::ParseError(error.to_string())
+    }
+
+    pub fn server(error: impl Display) -> Error {
+        Error::ServerError(error.to_string())
+    }
+
+    pub fn storage(error: impl Display) -> Error {
+        Error::StorageError(error.to_string())
+    }
+
+    pub fn message(text: impl Into<String>) -> Error {
+        Error::Message(text.into())
+    }
+
+    pub fn custom(error: Box<dyn std::error::Error>) -> Error {
+        Error::Custom(format!("{}", error))
+    }
+}
+
+impl From<Box<dyn std::error::Error>> for Error {
+    fn from(err: Box<dyn std::error::Error>) -> Self {
+        Error::custom(err)
+    }
+}
---
+++ src/storage/mod.rs
@@ -0,0 +1,3 @@
+mod cursor;
+
+pub use cursor::*;
