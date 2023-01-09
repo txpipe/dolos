3fc6303e4e4c33480693d76949600be6f6dd7668
Santiago Carmuega <santiago@carmuega.me>
2023-01-09 22:33:31 +0100

    fix: Fix test concurrency issues (#18)

--- src/tests/plexing.rs
+++ src/tests/plexing.rs
@@ -1,19 +1,10 @@
-use pallas::network::miniprotocols::Point;
-use tracing::info;
-
 use crate::upstream::chainsync;
 use crate::upstream::plexer;
 use crate::upstream::prelude::*;
+use pallas::network::miniprotocols::Point;
 
 #[test]
 fn connect_to_real_relay() {
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::FmtSubscriber::builder()
-            .with_max_level(tracing::Level::TRACE)
-            .finish(),
-    )
-    .unwrap();
-
     let known_point = Point::Specific(
         3866155,
         hex::decode("9a5446c4178c708706f8218ee05cec7674396e5f044f911eacc1ad1147cc353e").unwrap(),
@@ -39,7 +30,6 @@ fn connect_to_real_relay() {
 
     assert_eq!(point.unwrap(), known_point);
 
-    info!("dismissing");
     tether.dismiss_stage().unwrap();
     tether.join_stage();
 }
