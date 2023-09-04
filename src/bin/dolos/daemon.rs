911b8888ca589571573810fd85c41e6d66132f53
paulobressan <paulo.bressan@outlook.com>
2023-09-04 14:53:22 -0300

    feat: added retry config

--- src/bin/dolos/daemon.rs
+++ src/bin/dolos/daemon.rs
@@ -9,7 +9,11 @@ use dolos::{
 pub struct Args {}
 
 #[tokio::main]
-pub async fn run(config: super::Config, _args: &Args) -> Result<(), Error> {
+pub async fn run(
+    config: super::Config,
+    policy: &gasket::runtime::Policy,
+    _args: &Args,
+) -> Result<(), Error> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::builder()
             .with_max_level(tracing::Level::INFO)
@@ -44,7 +48,7 @@ pub async fn run(config: super::Config, _args: &Args) -> Result<(), Error> {
         from_sync.try_into().unwrap(),
     ));
 
-    dolos::sync::pipeline(&config.upstream, rolldb, applydb, to_serve)
+    dolos::sync::pipeline(&config.upstream, rolldb, applydb, to_serve, policy)
         .unwrap()
         .block();
 
