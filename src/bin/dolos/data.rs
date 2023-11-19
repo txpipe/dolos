d85a7fd527bd1bc8d48cde0db02459a6cc8cc7f4
Santiago Carmuega <santiago@carmuega.me>
2023-11-19 17:23:59 -0300

    feat: introduce eval command

--- src/bin/dolos/data.rs
+++ src/bin/dolos/data.rs
@@ -1,3 +1,30 @@
+use std::path::Path;
+
+use miette::IntoDiagnostic;
+
+use pallas::{
+    ledger::traverse::{tx, MultiEraBlock},
+    storage::rolldb::chain,
+};
+
+fn dump_txs(chain: &chain::Store) -> miette::Result<()> {
+    for header in chain.crawl() {
+        let (slot, hash) = header.into_diagnostic()?;
+        println!("dumping {slot}");
+
+        let block = chain.get_block(hash).into_diagnostic()?.unwrap();
+        let block = MultiEraBlock::decode(&block).into_diagnostic()?;
+
+        for tx in block.txs() {
+            let cbor = hex::encode(tx.encode());
+            let path = format!("{}.tx", tx.hash());
+            std::fs::write(Path::new(&path), cbor).into_diagnostic()?;
+        }
+    }
+
+    Ok(())
+}
+
 #[derive(Debug, clap::Args)]
 pub struct Args {}
 
@@ -30,6 +57,11 @@ pub fn run(config: &super::Config, _args: &Args) -> miette::Result<()> {
     } else {
         println!("chain is empty");
     }
+
+    // WIP utility to dump tx data for debugging purposes. Should be implemented as
+    // a subcommand.
+
+    // dump_txs(&chain)?;
 
     Ok(())
 }
