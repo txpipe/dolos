9c4d5647761be16305cb9154b932eb9c9ed76954
jmhrpr <25673452+jmhrpr@users.noreply.github.com>
2024-01-23 13:07:35 +0000

    feat: intersect in sync follow tip

--- src/serve/grpc/sync.rs
+++ src/serve/grpc/sync.rs
@@ -129,8 +129,37 @@ impl chain_sync_service_server::ChainSyncService for ChainSyncServiceImpl {
 
     async fn follow_tip(
         &self,
-        _request: Request<FollowTipRequest>,
+        request: Request<FollowTipRequest>,
     ) -> Result<Response<Self::FollowTipStream>, tonic::Status> {
+        let request = request.into_inner();
+
+        let has_intersect = !request.intersect.is_empty();
+
+        for intersect_attempt in request.intersect {
+            let slot = intersect_attempt.index;
+            let hash: [u8; 32] = intersect_attempt.hash.to_vec().try_into().unwrap();
+
+            let wal_seq = match self.wal.find_wal_seq(Some((slot, hash.into()))) {
+                Ok(x) => x,
+                Err(_) => continue, // TODO, not found is an error, error type (kvtable) not public. pallas find_wal_seq should not accept option.
+            };
+
+            if let Some(x) = wal_seq {
+                // TODO: race cond? WAL may have pruned our entry (and the
+                // following which we want) since we found the seq above
+                // TODO: combine find_wal_seq/rollstream functionality
+                let s = wal::RollStream::start_after(self.wal.clone(), Some(x))
+                    .map(|log| Ok(roll_to_tip_response(log)));
+
+                return Ok(Response::new(Box::pin(s)));
+            }
+        }
+
+        // intersects were provided but we couldn't intersect WAL using them
+        if has_intersect {
+            return Err(Status::not_found("could not find intersect"));
+        }
+
         let s = wal::RollStream::start_after(self.wal.clone(), None)
             .map(|log| Ok(roll_to_tip_response(log)));
 
