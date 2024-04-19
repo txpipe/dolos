341a5f6f2b5993bfd5ef43cd093ece84ab6c49c1
Santiago Carmuega <santiago@carmuega.me>
2024-04-18 23:49:04 -0300

    feat: implement basic u5c query module (#205)

--- src/bin/dolos/doctor/rebuild_ledger.rs
+++ src/bin/dolos/doctor/rebuild_ledger.rs
@@ -17,11 +17,15 @@ pub fn run(config: &crate::Config, _args: &Args) -> miette::Result<()> {
     let (_, chain, mut ledger) = open_data_stores(config).context("opening data stores")?;
 
     if ledger.is_empty() {
+        debug!("importing genesis");
+
         let byron_genesis =
             pallas::ledger::configs::byron::from_file(&config.byron.path).map_err(Error::config)?;
 
+        let delta = dolos::ledger::compute_origin_delta(&byron_genesis);
+
         ledger
-            .apply(&[dolos::ledger::compute_origin_delta(&byron_genesis)])
+            .apply(&[delta])
             .into_diagnostic()
             .context("applying origin utxos")?;
     }
@@ -33,33 +37,30 @@ pub fn run(config: &crate::Config, _args: &Args) -> miette::Result<()> {
 
     let remaining = chain.crawl_after(cursor.map(|x| x.0));
 
-    for chunk in remaining.chunks(200).into_iter() {
-        let deltas = chunk.map(|point| -> miette::Result<LedgerDelta> {
-            let (_, hash) = point.into_diagnostic().context("crawling chain")?;
+    for point in remaining {
+        let (slot, hash) = point.into_diagnostic().context("crawling chain")?;
+        debug!(slot, "importing block");
 
-            let block = chain
-                .get_block(hash)
-                .into_diagnostic()
-                .context("reading block")?
-                .ok_or(miette::miette!("block not found"))?;
+        let block = chain
+            .get_block(hash)
+            .into_diagnostic()
+            .context("reading block")?
+            .ok_or(miette::miette!("block not found"))?;
 
-            let blockd = MultiEraBlock::decode(&block)
-                .into_diagnostic()
-                .context("decoding block cbor")?;
+        let blockd = MultiEraBlock::decode(&block)
+            .into_diagnostic()
+            .context("decoding block cbor")?;
 
-            Ok(dolos::ledger::compute_delta(&blockd))
-        });
+        let context = dolos::ledger::load_slice_for_block(&blockd, &ledger)
+            .into_diagnostic()
+            .context("loading block context")?;
 
-        let deltas: Vec<_> = deltas.try_collect()?;
-
-        if let Some(last) = deltas.last() {
-            if let Some(ChainPoint(slot, _)) = &last.new_position {
-                debug!(slot, "importing block");
-            }
-        };
+        let delta = dolos::ledger::compute_delta(&blockd, context)
+            .into_diagnostic()
+            .context("computing ledger delta for block")?;
 
         ledger
-            .apply(deltas.as_slice())
+            .apply(&[delta])
             .into_diagnostic()
             .context("applying ledger block")?;
     }
