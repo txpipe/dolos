347d83d9fb69de4f8f59273134313c4fd85d6871
Santiago Carmuega <santiago@carmuega.me>
2024-05-25 09:28:56 -0300

    feat: add wal integrity doctor utility (#240)

--- src/bin/dolos/doctor/mod.rs
+++ src/bin/dolos/doctor/mod.rs
@@ -1,11 +1,14 @@
 use clap::{Parser, Subcommand};
 
 mod rebuild_ledger;
+mod wal_integrity;
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
     /// rebuilds the whole ledger from chain data
     RebuildLedger(rebuild_ledger::Args),
+    /// checks the integrity of the WAL records
+    WalIntegrity(wal_integrity::Args),
 }
 
 #[derive(Debug, Parser)]
@@ -17,6 +20,7 @@ pub struct Args {
 pub fn run(config: &super::Config, args: &Args) -> miette::Result<()> {
     match &args.command {
         Command::RebuildLedger(x) => rebuild_ledger::run(config, x)?,
+        Command::WalIntegrity(x) => wal_integrity::run(config, x)?,
     }
 
     Ok(())
