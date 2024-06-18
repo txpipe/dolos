30947d83c377975b4792a2ebf5ac88fbb86f5dbf
Santiago Carmuega <santiago@carmuega.me>
2024-06-18 14:37:34 -0300

    handle windows

--- src/bin/dolos/common.rs
+++ src/bin/dolos/common.rs
@@ -89,23 +89,34 @@ pub fn open_genesis_files(config: &GenesisConfig) -> miette::Result<GenesisFiles
     Ok((byron_genesis, shelley_genesis, alonzo_genesis))
 }
 
+#[inline]
+#[cfg(unix)]
+async fn wait_for_exit_signal() {
+    let mut sigterm =
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()).unwrap();
+
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => {
+            warn!("SIGINT detected");
+        }
+        _ = sigterm.recv() => {
+            warn!("SIGTERM detected");
+        }
+    };
+}
+
+#[inline]
+#[cfg(windows)]
+async fn wait_for_exit_signal() {
+    tokio::signal::ctrl_c().await.unwrap()
+}
+
 pub fn hook_exit_token() -> CancellationToken {
     let cancel = CancellationToken::new();
 
     let cancel2 = cancel.clone();
     tokio::spawn(async move {
-        let mut sigterm =
-            tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()).unwrap();
-
-        tokio::select! {
-            _ = tokio::signal::ctrl_c() => {
-                warn!("SIGINT detected");
-            }
-            _ = sigterm.recv() => {
-                warn!("SIGTERM detected");
-            }
-        };
-
+        wait_for_exit_signal().await;
         debug!("notifying exit");
         cancel2.cancel();
     });
