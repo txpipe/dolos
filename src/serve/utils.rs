13cd9b3e2e60c82128980c503e968a9d259cbb87
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-08-05 22:48:14 -0300

    Improve utils and test

--- src/serve/utils.rs
+++ src/serve/utils.rs
@@ -1,8 +1,7 @@
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::{json, Value};
 
-// Helper function to get a nested value from a serde_json::Value
-pub fn get_nested_value(value: &Value, keys: &[&str]) -> Option<Value> {
+fn get_nested_value(value: &Value, keys: &[&str]) -> Option<Value> {
     let mut current = value;
     for key in keys {
         match current {
@@ -13,13 +12,12 @@ pub fn get_nested_value(value: &Value, keys: &[&str]) -> Option<Value> {
     Some(current.clone())
 }
 
-// Helper function to set a nested value in a serde_json::Value
-pub fn set_nested_value(value: &mut Value, keys: &[&str], new_value: Value) {
+fn set_nested_value(value: &mut Value, keys: &[&str], new_value: Value) {
     let mut current = value;
     for key in keys.iter().take(keys.len() - 1) {
         current = current
             .as_object_mut()
-            .unwrap()
+            .unwrap() // Safe to unwrap when used in conjunction with get_nested_value
             .entry(key.to_string())
             .or_insert_with(|| json!({}));
     }
@@ -48,18 +46,37 @@ where
 
 #[cfg(test)]
 mod tests {
+    use serde::Deserialize;
+
     use super::*;
+
+    #[derive(Deserialize, Serialize, Default, Debug, PartialEq, Clone)]
+    #[serde(default)]
+    struct Bar {
+        pub abc: usize,
+    }
+
+    #[derive(Deserialize, Serialize, Default, Debug, Clone)]
+    #[serde(default)]
+    struct Foo {
+        pub foo: String,
+        pub bar: Bar,
+    }
 
     #[test]
     fn test_apply_mask() {
-        let pparams = pallas::interop::utxorpc::spec::cardano::PParams {
-            coins_per_utxo_byte: 10,
-            max_tx_size: 10,
-            ..Default::default()
+        let foobar = Foo {
+            foo: "foo".into(),
+            bar: Bar { abc: 10 },
         };
-        let paths = vec!["coinsPerUtxoByte".to_string()];
-        let new_pparams = apply_mask(pparams, paths).expect("Failed to serde");
-        assert_eq!(new_pparams.coins_per_utxo_byte, 10);
-        assert_eq!(new_pparams.max_tx_size, 0);
+        let paths = vec!["foo".to_string()];
+        let masked_foobar = apply_mask(foobar.clone(), paths).expect("Failed to serde");
+        assert_eq!(masked_foobar.foo, "foo");
+        assert_eq!(masked_foobar.bar, Bar::default());
+
+        let paths = vec!["bar".to_string()];
+        let masked_foobar = apply_mask(foobar, paths).expect("Failed to serde");
+        assert_eq!(masked_foobar.foo, String::default());
+        assert_eq!(masked_foobar.bar.abc, 10);
     }
 }
