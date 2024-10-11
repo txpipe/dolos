39acf5d4bf83806697e79793ef85efcb309a11b0
Santiago Carmuega <santiago@carmuega.me>
2024-10-11 09:27:01 -0300

    chore(deps): bump tonic to v0.12 which requires bump on u5c and pallas (#345)

--- src/serve/grpc/mod.rs
+++ src/serve/grpc/mod.rs
@@ -56,7 +56,7 @@ pub async fn serve(
         .register_encoded_file_descriptor_set(u5c::submit::FILE_DESCRIPTOR_SET)
         .register_encoded_file_descriptor_set(u5c::watch::FILE_DESCRIPTOR_SET)
         .register_encoded_file_descriptor_set(protoc_wkt::google::protobuf::FILE_DESCRIPTOR_SET)
-        .build()
+        .build_v1()
         .unwrap();
 
     let cors_layer = if config.cors_enabled {
