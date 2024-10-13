9cf565cd48ea6143dd6cfd69f33353a1f9f34835
Santiago Carmuega <santiago@carmuega.me>
2024-10-13 16:00:47 -0300

    more

--- src/ledger/pparams/mod.rs
+++ src/ledger/pparams/mod.rs
@@ -441,7 +441,7 @@ mod tests {
         // in order
         let blocks: Vec<_> = files
             .iter()
-            .map(|x| MultiEraBlock::decode(&x).unwrap())
+            .map(|x| MultiEraBlock::decode(x).unwrap())
             .sorted_by_key(|b| b.slot())
             .collect();
 
