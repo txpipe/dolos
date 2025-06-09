5bf5fbbbaa92286e7acc46623e095a93e977c546
Santiago Carmuega <santiago@carmuega.me>
2025-06-09 08:50:28 -0300

    feat: add reset-genesis command (#532)

---
+++ src/bin/dolos/doctor/reset_genesis.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use crate::init::{include, KnownNetwork};
+
+#[derive(Debug, clap::Args)]
+pub struct Args {
+    #[arg(long)]
+    pub network: Option<KnownNetwork>,
+}
+
+fn infer_network(config: &crate::Config) -> Option<KnownNetwork> {
+    let magic = config.upstream.network_magic()?;
+    KnownNetwork::from_magic(magic)
+}
+
+pub fn run(config: &crate::Config, args: &Args) -> miette::Result<()> {
+    let infered = infer_network(config);
+
+    let network = match (&args.network, &infered) {
+        (Some(network), _) => network,
+        (None, Some(infered)) => infered,
+        (None, None) => {
+            return Err(miette::Error::msg(
+                "no network specified and can't infer it from the config",
+            ))
+        }
+    };
+
+    include::save_genesis_configs(&PathBuf::from("./"), network)?;
+
+    Ok(())
+}
