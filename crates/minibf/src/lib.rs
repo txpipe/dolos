6b74815c01f15e80fd73581c94d587695dccf7ae
Santiago Carmuega <santiago@carmuega.me>
2025-07-28 07:06:44 -0300

    feat(minibf): implement accounts/{stake_address}/delegations endpoint

--- crates/minibf/src/lib.rs
+++ crates/minibf/src/lib.rs
@@ -134,6 +134,10 @@ impl<D: Domain, C: CancelToken> dolos_core::Driver<D, C> for Driver {
                 get(routes::accounts::by_stake::<D>),
             )
             .route(
+                "/accounts/{stake_address}/delegations",
+                get(routes::accounts::by_stake_delegations::<D>),
+            )
+            .route(
                 "/accounts/{stake_address}/addresses",
                 get(routes::accounts::by_stake_addresses::<D>),
             )
