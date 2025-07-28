afe32881c7929f2eed54cb167c8ae02a5462b670
Santiago Carmuega <santiago@carmuega.me>
2025-07-28 04:39:54 -0300

    feat(minibf): implement /network endpoint

--- crates/minibf/src/routes/network.rs
+++ crates/minibf/src/routes/network.rs
@@ -2,13 +2,18 @@ use std::ops::Sub;
 
 use axum::{extract::State, http::StatusCode, Json};
 use blockfrost_openapi::models::{
-    network_eras_inner::NetworkErasInner, network_eras_inner_end::NetworkErasInnerEnd,
+    network::Network, network_eras_inner::NetworkErasInner,
+    network_eras_inner_end::NetworkErasInnerEnd,
     network_eras_inner_parameters::NetworkErasInnerParameters,
-    network_eras_inner_start::NetworkErasInnerStart,
+    network_eras_inner_start::NetworkErasInnerStart, network_stake::NetworkStake,
+    network_supply::NetworkSupply,
 };
 use chrono::{DateTime, FixedOffset};
-use dolos_cardano::pparams::{ChainSummary, EraSummary};
-use dolos_core::{Domain, Genesis};
+use dolos_cardano::{
+    model::{EpochState, CURRENT_EPOCH_KEY},
+    pparams::{ChainSummary, EraSummary},
+};
+use dolos_core::{ArchiveStore as _, Domain, Genesis, State3Store};
 
 use crate::{mapping::IntoModel, Facade};
 
@@ -89,6 +94,52 @@ pub async fn eras<D: Domain>(
     let genesis = domain.genesis();
 
     let builder = ChainModelBuilder { chain, genesis };
+
+    builder.into_response()
+}
+
+struct NetworkModelBuilder<'a> {
+    genesis: &'a Genesis,
+    state: EpochState,
+}
+
+impl<'a> IntoModel<Network> for NetworkModelBuilder<'a> {
+    type SortKey = ();
+
+    fn into_model(self) -> Result<Network, StatusCode> {
+        let max_supply = self.genesis.shelley.max_lovelace_supply.unwrap_or_default();
+        let total_supply = self.state.supply_circulating + self.state.supply_locked;
+        let reserves = max_supply - total_supply;
+
+        Ok(Network {
+            supply: Box::new(NetworkSupply {
+                max: max_supply.to_string(),
+                total: total_supply.to_string(),
+                circulating: self.state.supply_circulating.to_string(),
+                locked: self.state.supply_locked.to_string(),
+                treasury: self.state.treasury.to_string(),
+                reserves: reserves.to_string(),
+            }),
+            stake: Box::new(NetworkStake {
+                live: self.state.stake_live.to_string(),
+                active: self.state.stake_active.to_string(),
+            }),
+        })
+    }
+}
+
+pub async fn naked<D: Domain>(
+    State(domain): State<Facade<D>>,
+) -> Result<Json<Network>, StatusCode> {
+    let genesis = domain.genesis();
+
+    let state = domain
+        .state3()
+        .read_entity_typed::<EpochState>(CURRENT_EPOCH_KEY)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::SERVICE_UNAVAILABLE)?;
+
+    let builder = NetworkModelBuilder { genesis, state };
 
     builder.into_response()
 }
