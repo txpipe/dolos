d9f8b11f7a3057d9d18b042e5c9bf67effa19f7a
paulobressan <paulo.bressan@outlook.com>
2025-08-02 17:53:55 -0300

    fix: fixed mempool script error logs

--- src/mempool.rs
+++ src/mempool.rs
@@ -285,7 +285,7 @@ impl MempoolStore for Mempool {
 
             for eval in report.iter() {
                 if !eval.success {
-                    return Err(MempoolError::Phase2ExplicitError(eval.logs));
+                    return Err(MempoolError::Phase2ExplicitError(eval.logs.clone()));
                 }
             }
         }
