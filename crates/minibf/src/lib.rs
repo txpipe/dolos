03d33cdfd8e377be7bd8c820d6142ad43016e598
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-09-09 15:26:39 -0300

    chore(minibf): add root and health endpoints (#663)

--- crates/minibf/src/lib.rs
+++ crates/minibf/src/lib.rs
@@ -39,6 +39,7 @@ pub struct Config {
     pub permissive_cors: Option<bool>,
     pub metadata_max_scan_depth: Option<usize>,
     pub token_registry_url: Option<String>,
+    pub url: Option<String>,
 }
 
 #[derive(Clone)]
@@ -180,6 +181,9 @@ where
 
     async fn run(cfg: Self::Config, domain: D, cancel: C) -> Result<(), ServeError> {
         let app = Router::new()
+            .route("/", get(routes::root::<D>))
+            .route("/health", get(routes::health::naked))
+            .route("/health/clock", get(routes::health::clock))
             .route("/genesis", get(routes::genesis::naked::<D>))
             .route("/network", get(routes::network::naked::<D>))
             .route("/network/eras", get(routes::network::eras::<D>))
--- crates/minibf/src/routes/mod.rs
+++ crates/minibf/src/routes/mod.rs
@@ -5,9 +5,45 @@ pub mod blocks;
 pub mod epochs;
 pub mod genesis;
 pub mod governance;
+pub mod health;
 pub mod metadata;
 pub mod network;
 pub mod pools;
 pub mod tx;
 pub mod txs;
 pub mod utxos;
+
+use std::env;
+
+use axum::{extract::State, http::StatusCode, Json};
+use dolos_core::Domain;
+use serde::{Deserialize, Serialize};
+
+use crate::{Config, Facade};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RootResponse {
+    url: String,
+    version: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    revision: Option<String>,
+}
+
+impl From<&Config> for RootResponse {
+    fn from(value: &Config) -> Self {
+        Self {
+            url: value
+                .url
+                .clone()
+                .unwrap_or(value.listen_address.to_string()),
+            version: env!("CARGO_PKG_VERSION").to_string(),
+            revision: option_env!("GIT_REVISION").map(|x| x.to_string()),
+        }
+    }
+}
+
+pub async fn root<D: Domain>(
+    State(domain): State<Facade<D>>,
+) -> Result<Json<RootResponse>, StatusCode> {
+    Ok(Json(RootResponse::from(&domain.config)))
+}
