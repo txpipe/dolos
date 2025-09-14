a0b14f0bd7350093fc58b649cb828ff573ee5d2e
Santiago Carmuega <santiago@carmuega.me>
2025-09-14 19:27:18 -0300

    feat(cli): add clear-state data command (#677)

---
+++ src/bin/dolos/data/clear_state.rs
@@ -0,0 +1,52 @@
+use std::marker::PhantomData;
+
+use comfy_table::Table;
+use dolos_cardano::{
+    model::AccountState, EpochState, EraSummary, FixedNamespace as _, PoolState, RewardLog,
+    EPOCH_KEY_GO, EPOCH_KEY_MARK, EPOCH_KEY_SET,
+};
+use itertools::Itertools as _;
+use miette::{bail, Context, IntoDiagnostic};
+
+use dolos::prelude::*;
+
+#[derive(Debug, clap::Args)]
+pub struct Args {
+    /// namespace to dump
+    #[arg(long)]
+    namespace: String,
+}
+
+fn clear_state(config: &crate::Config, ns: Namespace) -> miette::Result<()> {
+    let state = crate::common::open_state_store(config)?;
+    let writer = state.start_writer().into_diagnostic()?;
+
+    let all_keys = state
+        .iter_entities(ns, full_range())
+        .into_diagnostic()
+        .context("iterating entities")?
+        .map_ok(|(key, _)| key);
+
+    for key in all_keys {
+        let key = key.into_diagnostic()?;
+        writer.delete_entity(ns, &key).into_diagnostic()?;
+    }
+
+    writer.commit().into_diagnostic()?;
+
+    Ok(())
+}
+
+pub fn run(config: &crate::Config, args: &Args) -> miette::Result<()> {
+    crate::common::setup_tracing(&config.logging)?;
+
+    match args.namespace.as_str() {
+        "eras" => clear_state(config, "eras")?,
+        "epochs" => clear_state(config, "epochs")?,
+        "accounts" => clear_state(config, "accounts")?,
+        "pools" => clear_state(config, "pools")?,
+        _ => todo!(),
+    }
+
+    Ok(())
+}
