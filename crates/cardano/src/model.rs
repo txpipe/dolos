bd9eecad50e6776eb348cce45344a854da36e89d
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-09-19 16:31:43 -0300

    chore: Fix drep handling

--- crates/cardano/src/model.rs
+++ crates/cardano/src/model.rs
@@ -32,7 +32,7 @@ use crate::{
             StakeRegistration, VoteDelegation, WithdrawalInc,
         },
         assets::MintStatsUpdate,
-        dreps::{DRepRegistration, DRepUnRegistration},
+        dreps::{DRepActivity, DRepRegistration, DRepUnRegistration},
         epochs::{EpochStatsUpdate, NoncesUpdate, PParamsUpdate},
         pools::{MintedBlocksInc, PoolRegistration},
     },
@@ -958,6 +958,7 @@ pub enum CardanoDelta {
     EpochStatsUpdate(EpochStatsUpdate),
     DRepRegistration(DRepRegistration),
     DRepUnRegistration(DRepUnRegistration),
+    DRepActivity(DRepActivity),
     WithdrawalInc(WithdrawalInc),
     VoteDelegation(VoteDelegation),
     PParamsUpdate(PParamsUpdate),
@@ -1009,6 +1010,7 @@ delta_from!(MintStatsUpdate);
 delta_from!(EpochStatsUpdate);
 delta_from!(DRepRegistration);
 delta_from!(DRepUnRegistration);
+delta_from!(DRepActivity);
 delta_from!(WithdrawalInc);
 delta_from!(VoteDelegation);
 delta_from!(PParamsUpdate);
@@ -1029,6 +1031,7 @@ impl dolos_core::EntityDelta for CardanoDelta {
             Self::MintStatsUpdate(x) => x.key(),
             Self::EpochStatsUpdate(x) => x.key(),
             Self::DRepRegistration(x) => x.key(),
+            Self::DRepActivity(x) => x.key(),
             Self::DRepUnRegistration(x) => x.key(),
             Self::WithdrawalInc(x) => x.key(),
             Self::VoteDelegation(x) => x.key(),
@@ -1050,6 +1053,7 @@ impl dolos_core::EntityDelta for CardanoDelta {
             Self::EpochStatsUpdate(x) => Self::downcast_apply(x, entity),
             Self::DRepRegistration(x) => Self::downcast_apply(x, entity),
             Self::DRepUnRegistration(x) => Self::downcast_apply(x, entity),
+            Self::DRepActivity(x) => Self::downcast_apply(x, entity),
             Self::WithdrawalInc(x) => Self::downcast_apply(x, entity),
             Self::VoteDelegation(x) => Self::downcast_apply(x, entity),
             Self::PParamsUpdate(x) => Self::downcast_apply(x, entity),
@@ -1070,6 +1074,7 @@ impl dolos_core::EntityDelta for CardanoDelta {
             Self::EpochStatsUpdate(x) => Self::downcast_undo(x, entity),
             Self::DRepRegistration(x) => Self::downcast_undo(x, entity),
             Self::DRepUnRegistration(x) => Self::downcast_undo(x, entity),
+            Self::DRepActivity(x) => Self::downcast_undo(x, entity),
             Self::WithdrawalInc(x) => Self::downcast_undo(x, entity),
             Self::VoteDelegation(x) => Self::downcast_undo(x, entity),
             Self::PParamsUpdate(x) => Self::downcast_undo(x, entity),
