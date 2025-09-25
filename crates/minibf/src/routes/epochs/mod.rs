7c5cb51c824d4502e542a2837878259eb7ac7045
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-09-25 18:42:27 -0300

    chore: Log epochs on sweep

--- crates/minibf/src/routes/epochs/mod.rs
+++ crates/minibf/src/routes/epochs/mod.rs
@@ -6,16 +6,16 @@ use axum::{
 use blockfrost_openapi::models::epoch_param_content::EpochParamContent;
 
 use dolos_cardano::{EpochState, FixedNamespace, EPOCH_KEY_SET};
-use dolos_core::{Domain, StateStore};
+use dolos_core::{ArchiveStore, Domain, LogKey, StateStore, TemporalKey};
 
-use crate::{mapping::IntoModel as _, Facade};
+use crate::{error::Error, mapping::IntoModel as _, Facade};
 
 pub mod cost_models;
 pub mod mapping;
 
 pub async fn latest_parameters<D: Domain>(
     State(domain): State<Facade<D>>,
-) -> Result<Json<EpochParamContent>, StatusCode> {
+) -> Result<Json<EpochParamContent>, Error> {
     let tip = domain.get_tip_slot()?;
 
     let summary = domain.get_chain_summary()?;
@@ -38,21 +38,31 @@ pub async fn latest_parameters<D: Domain>(
         nonce,
     };
 
-    model.into_response()
+    Ok(model.into_response()?)
 }
 
 pub async fn by_number_parameters<D: Domain>(
     State(domain): State<Facade<D>>,
     Path(epoch): Path<u64>,
-) -> Result<Json<EpochParamContent>, StatusCode> {
-    let params = domain.get_live_pparams()?;
+) -> Result<Json<EpochParamContent>, Error> {
+    let epoch_start = domain.get_chain_summary()?.epoch_start(epoch);
+    let state = domain
+        .archive()
+        .read_log_typed::<EpochState>(
+            EpochState::NS,
+            &LogKey::from(TemporalKey::from(epoch_start)),
+        )
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::NOT_FOUND)?;
+    let params = state.pparams;
+    let nonce = state.nonces.map(|x| x.active.to_string());
 
     let model = mapping::ParametersModelBuilder {
         epoch,
         params,
         genesis: domain.genesis(),
-        nonce: None,
+        nonce,
     };
 
-    model.into_response()
+    Ok(model.into_response()?)
 }
