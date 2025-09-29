3c693c36f39727a9664570cdfc7827281f762608
Felipe Gonzalez <gonzalezzfelipe@gmail.com>
2025-09-29 20:17:25 -0300

    feat(minibf): implement more `/assets/{asset}/*` endpoints (#735)

--- crates/minibf/src/lib.rs
+++ crates/minibf/src/lib.rs
@@ -361,6 +361,14 @@ where
             )
             .route("/assets/{subject}", get(routes::assets::by_subject::<D>))
             .route(
+                "/assets/{subject}/addresses",
+                get(routes::assets::by_subject_addresses::<D>),
+            )
+            .route(
+                "/assets/{subject}/transactions",
+                get(routes::assets::by_subject_transactions::<D>),
+            )
+            .route(
                 "/metadata/txs/labels/{label}",
                 get(routes::metadata::by_label_json::<D>),
             )
