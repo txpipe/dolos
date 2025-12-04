8c1f53f3e7aa2af0aeaf4e6f3f6a2a947558f626
Santiago Carmuega <santiago@carmuega.me>
2025-12-04 09:57:51 -0300

    fix: apply pending refactors to windows code

--- src/serve/o7s_win/mod.rs
+++ src/serve/o7s_win/mod.rs
@@ -1,14 +1,11 @@
-use std::path::PathBuf;
-
-use serde::{Deserialize, Serialize};
 use tracing::{error, instrument};
 
-use dolos_core::{CancelToken, Domain, ServeError};
+use dolos_core::{config::OuroborosConfig, CancelToken, Domain, ServeError};
 
 pub struct Driver;
 
 impl<D: Domain, C: CancelToken> dolos_core::Driver<D, C> for Driver {
-    type Config = Config;
+    type Config = OuroborosConfig;
 
     #[instrument(skip_all)]
     async fn run(_cfg: Self::Config, _domain: D, _cancel: C) -> Result<(), ServeError> {
