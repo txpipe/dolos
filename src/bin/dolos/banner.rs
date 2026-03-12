2c283856c1a059c8cda1ed4964cd5558d1598070
Santiago <santiago@carmuega.me>
2026-03-12 17:20:44 -0300

    use vergen

--- src/bin/dolos/banner.rs
+++ src/bin/dolos/banner.rs
@@ -11,7 +11,7 @@ pub fn print_init_banner() {
 
     println!("\x1b[90moooooooooooooooooooooooooooooooooooooooo\x1b[0m");
 
-    let git_sha = option_env!("DOLOS_GIT_SHA").unwrap_or("unknown");
+    let git_sha = option_env!("VERGEN_GIT_SHA").unwrap_or("unknown");
 
     println!(
         "\x1b[1;95mv{} ({})\x1b[0m\n",
