56036ed3e2e43980ff4ec59692afbdcb98c72cce
Nicolas Ludueña <luduena.nicolas.victorio@gmail.com>
2026-03-16 11:43:26 -0300

    feat: create hash struct

---
+++ crates/core/src/hash.rs
@@ -0,0 +1,59 @@
+use serde::{Deserialize, Serialize};
+use std::{fmt, str::FromStr};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct Hash<const N: usize>([u8; N]);
+
+impl<const N: usize> Hash<N> {
+    pub fn new(bytes: [u8; N]) -> Hash<N> {
+        Hash(bytes)
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl<const N: usize> fmt::Display for Hash<N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", hex::encode(self.0))
+    }
+}
+
+impl<const N: usize> FromStr for Hash<N> {
+    type Err = hex::FromHexError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = hex::decode(s)?;
+        if bytes.len() != N {
+            return Err(hex::FromHexError::InvalidStringLength);
+        }
+        let mut arr = [0u8; N];
+
+        arr.copy_from_slice(&bytes);
+        Ok(Self(arr))
+    }
+}
+
+impl<const N: usize> Serialize for Hash<N> {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        s.serialize_str(&hex::encode(self.0))
+    }
+}
+
+impl<'de, const N: usize> Deserialize<'de> for Hash<N> {
+    fn deserialize<D>(d: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(d)?;
+        s.parse().map_err(serde::de::Error::custom)
+    }
+}
+
+// TODO: add more methods here. Probably will need a bunch so hash is its own file
+//
+//
+//
